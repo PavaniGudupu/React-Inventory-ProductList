ec843dcf179bcd3796aa608fd118a40fe7313f22
G. Pavani <pavani9419@gmail.com>
2026-03-23 20:28:28 +0530

    Added SSL config for Render PostgreSQL

--- backend/DataModel/db.js
+++ backend/DataModel/db.js
@@ -8,7 +8,10 @@ const db = pgp({
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
     password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
 
 
