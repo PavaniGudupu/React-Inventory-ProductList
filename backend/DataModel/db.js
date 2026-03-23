083ab585e2e3a7cca151421ccd12ffc8360b5c73
G. Pavani <pavani9419@gmail.com>
2026-03-23 20:38:44 +0530

    Use connectionString for SSL fix

--- backend/DataModel/db.js
+++ backend/DataModel/db.js
@@ -2,17 +2,14 @@ import env from 'dotenv';
 import pgPromise from 'pg-promise';
 
 env.config();
-const pgp = pgPromise();
-const db = pgp({
-    user: process.env.DB_USER,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
-    ssl: {
-        rejectUnauthorized: false
-    }
-})
 
+const pgp = pgPromise();
+
+const db = pgp({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 export default db;
